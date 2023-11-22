0957086ca9fc9872568f4680eecc1fa457bfb229
jiorpilla <jiorpilla@gmail.com>
2023-11-23 04:02:58 +0800

    added Gallery CRU
    since no Delete

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -2,41 +2,61 @@
 
 namespace App\Controller;
 
-use App\Entity\Branch;
 use App\Entity\Gallery;
 use App\Form\GalleryType;
+use App\Message\GeneratePdfMessage;
+use App\Repository\GalleryRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/gallery', name: "gallery_")]
 class GalleryController extends AbstractController
 {
-    #[Route('/gallery', name: 'list')]
-    public function index(): Response
+    #[Route('/', name: 'list', methods: ['GET'])]
+    public function index(GalleryRepository $galleryRepository): Response
     {
+        //fetch all entities
+        $gallery = $galleryRepository->findAll();
+
+        // Check if the PDF files exist
+        foreach ($gallery as $item) {
+            $filesystem = new Filesystem();
+            $filePath = './pdf/' . $item->getId() . '.pdf';
+
+            //check if file exists in public folder
+            if ($filesystem->exists($filePath)) {
+                $item->pdfFilePath = 'pdf/' . $item->getId() . '.pdf';
+                $item->pdfExists = file_exists($item->pdfFilePath);
+            }
+        }
         return $this->render('gallery/index.html.twig', [
-            'controller_name' => 'GalleryController',
+            'gallery_list' => $gallery,
         ]);
     }
+
     #[Route('/create', name: 'create_gallery', methods: ['GET', 'POST'])]
-    public function createGallery(Request $request, EntityManagerInterface $entityManager): Response
+    public function createGallery(Request $request, EntityManagerInterface $entityManager, MessageBusInterface $bus): Response
     {
         $gallery = new Gallery();
         $form = $this->createForm(GalleryType::class, $gallery);
 
         $form->handleRequest($request);
 
-        dump($gallery);
         if ($form->isSubmitted() && $form->isValid()) {
-            foreach ($form->getData()->getImages() as $image){
+            foreach ($form->getData()->getImages() as $image) {
                 $entityManager->persist($image);
                 $gallery->addImage($image);
             }
             $entityManager->persist($gallery);
             $entityManager->flush();
+
+            //dispatch message
+            $bus->dispatch(new GeneratePdfMessage($gallery->getId()));
 
             return $this->redirectToRoute('gallery_list');
         }
@@ -47,7 +67,7 @@ class GalleryController extends AbstractController
     }
 
 
-    #[Route('/{id}', name: 'app_branch_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Gallery $gallery): Response
     {
         return $this->render('gallery/show.html.twig', [
@@ -56,16 +76,23 @@ class GalleryController extends AbstractController
     }
 
     #[Route('/edit/{id}', name: 'edit_gallery', methods: ['GET', 'POST'])]
-    public function edit(Request $request, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, EntityManagerInterface $entityManager, Gallery $gallery, MessageBusInterface $bus): Response
     {
-        $gallery = new Gallery();
         $form = $this->createForm(GalleryType::class, $gallery);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            foreach ($form->getData()->getImages() as $image) {
+                $entityManager->persist($image);
+                $gallery->addImage($image);
+            }
+            $entityManager->persist($gallery);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_branch_show', ['id' => $gallery->getId()], Response::HTTP_SEE_OTHER);
+            //dispatch message
+            $bus->dispatch(new GeneratePdfMessage($gallery->getId()));
+
+            return $this->redirectToRoute('gallery_show', ['id' => $gallery->getId()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('gallery/edit.html.twig', [
