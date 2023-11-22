955964a98a90b60caaa80bc31bb0d65b1eab9b19
jiorpilla <jiorpilla@gmail.com>
2023-11-23 04:04:10 +0800

    Updated Home Controller
    and also it's related twig

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,7 @@
 
 namespace App\Controller;
 
-use DateTime;
-use Dompdf\Dompdf;
+use App\Service\GalleryPdfGenerator;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,41 +16,19 @@ class HomeController extends AbstractController
         ]);
     }
 
+    /**
+     * this was just created to test the GalleryPdfGenerator service
+     * @param GalleryPdfGenerator $pdfGenerator
+     * @return Response
+     */
     #[Route('/generate_pdf', name: 'app_pdf_generator')]
-    public function pdf_generator(): Response
+    public function pdf_generator(GalleryPdfGenerator $pdfGenerator): Response
     {
-//        $data = [
-//            'imageSrc' => $this->imageToBase64($this->getParameter('kernel.project_dir') . '/public/img/profile.png'),
-//            'name' => 'John Doe',
-//            'address' => 'USA',
-//            'mobileNumber' => '000000000',
-//            'email' => 'john.doe@email.com',
-//        ];
-        $html = $this->renderView('pdf/index.html.twig');
-        $dompdf = new Dompdf();
-        $dompdf->loadHtml($html);
-        $dompdf->render();
 
-        $output = $dompdf->output();
-        $pdfDirectory = $this->getParameter('kernel.project_dir') . '/public/pdf';
-        $filename = (new DateTime())->format("YmdHis");
-        // Check if the directory exists, create it if not
-        if (!is_dir($pdfDirectory)) {
-            mkdir($pdfDirectory, 0755, true);
-        }
-
-        $pdfFilePath = $pdfDirectory . '/ ' . $filename . '.pdf';
-        file_put_contents($pdfFilePath, $output);
+        $pdfGenerator->generatePdf(7);
 
         // You can return a response or redirect as needed
-        return new Response('PDF generated and saved at: ' . $pdfFilePath);
+        return new Response('PDF generated and saved');
     }
-
-//    private function imageToBase64($path) {
-//        $path = $path;
-//        $type = pathinfo($path, PATHINFO_EXTENSION);
-//        $data = file_get_contents($path);
-//        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
-//        return $base64;
-//    }
 }
+
